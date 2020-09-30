ef9f211c1bae3fc4469960eeee9e0f3e79d3910c
unknown <pablo.190777@gmail.com>
2020-09-30 18:47:52 -0300

    ajuste notacoes

--- backend/src/main/kotlin/dev3/estouropilha/trackr/backend/dto/EntregaDto.kt
+++ backend/src/main/kotlin/dev3/estouropilha/trackr/backend/dto/EntregaDto.kt
@@ -1,8 +1,12 @@
 package dev3.estouropilha.trackr.backend.dto
 
+import io.swagger.annotations.ApiModelProperty
+
 data class EntregaDto(
+        @field:ApiModelProperty(example = "11111111111")
         val cpf: String,
-        val movimentacoes: Iterable<MovimentacaoDto>,
+        val movimentacoes: List<MovimentacaoDto>,
+        @field:ApiModelProperty(example = "A1Z2")
         val codigo: String
 )
 
--- backend/src/main/kotlin/dev3/estouropilha/trackr/backend/dto/MovimentacaoDto.kt
+++ backend/src/main/kotlin/dev3/estouropilha/trackr/backend/dto/MovimentacaoDto.kt
@@ -5,11 +5,11 @@ import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalDateTime
 
 data class MovimentacaoDto(
-        @ApiModelProperty(example = "2020-09-28T21:01:30")
+        @field:ApiModelProperty(example = "2020-09-28T21:01:30")
         @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
         val dataHora: LocalDateTime,
-        @ApiModelProperty(example = "Centro de Distribuição XYZ")
+        @field:ApiModelProperty(example = "Centro de Distribuição XYZ")
         val local: String,
-        @ApiModelProperty(example = "Aguardando Separação")
+        @field:ApiModelProperty(example = "Aguardando Separação")
         val situacao: String
 )
