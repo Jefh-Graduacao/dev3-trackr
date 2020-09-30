b141eb6bc91728424a55286b0578c0599ab0cdfd
Jéf <buenojeferson@outlook.com>
2020-09-30 20:17:30 -0300

    ✨ Obter título (headline) da situação da movimentação

--- backend/src/main/kotlin/dev3/estouropilha/trackr/backend/models/Movimentacao.kt
+++ backend/src/main/kotlin/dev3/estouropilha/trackr/backend/models/Movimentacao.kt
@@ -2,4 +2,4 @@ package dev3.estouropilha.trackr.backend.models
 
 import java.time.LocalDateTime
 
-data class Movimentacao(val data: LocalDateTime, val unidade: String, val detalhes: String)
+data class Movimentacao(val titulo: String, val data: LocalDateTime, val unidade: String, val detalhes: String)
