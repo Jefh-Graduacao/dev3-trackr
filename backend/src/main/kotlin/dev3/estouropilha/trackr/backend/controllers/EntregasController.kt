7cb9b7baa56dea41013b7da0c41f24f69d305ca3
Jéf <buenojeferson@outlook.com>
2020-10-14 21:26:58 -0300

    ✨ Endpoint para consulta de entregas nos correios

--- backend/src/main/kotlin/dev3/estouropilha/trackr/backend/controllers/EntregasController.kt
+++ backend/src/main/kotlin/dev3/estouropilha/trackr/backend/controllers/EntregasController.kt
@@ -1,8 +1,10 @@
 package dev3.estouropilha.trackr.backend.controllers
 
+import dev3.estouropilha.trackr.backend.crawlers.CorreiosCrawler
 import dev3.estouropilha.trackr.backend.crawlers.SswCrawler
 import dev3.estouropilha.trackr.backend.dto.EntregaDto
 import dev3.estouropilha.trackr.backend.dto.MovimentacaoDto
+import dev3.estouropilha.trackr.backend.mappers.EntregasMapper
 import io.swagger.annotations.Api
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -13,7 +15,10 @@ import org.springframework.web.server.ResponseStatusException
 @RestController
 @Api("Entregas")
 @RequestMapping("/entregas")
-class EntregasController(private val sswCrawler: SswCrawler) {
+class EntregasController(
+        private val sswCrawler: SswCrawler,
+        private val correiosCrawler: CorreiosCrawler
+) {
     private val cpfRegex = "^\\d{11}$"
 
     @GetMapping("/{cpf}")
@@ -24,15 +29,20 @@ class EntregasController(private val sswCrawler: SswCrawler) {
         if (!Regex(cpfRegex).matches(cpf)) {
             throw ResponseStatusException(HttpStatus.BAD_REQUEST, "CPF inválido")
         }
+
         return ok(sswCrawler.consultarEntregas(cpf)
                 .map {
-                    EntregaDto(cpf,
-                            it.movimentacoes
-                                    .sortedByDescending { mov -> mov.data }
-                                    .map { movimentacao ->
-                                        MovimentacaoDto(movimentacao.titulo, movimentacao.data, movimentacao.unidade, movimentacao.detalhes)
-                                    }, "código")
+                    EntregasMapper.map(it, cpf, "")
                 }
         )
     }
+
+    @GetMapping("/correios/{codigoRastreio}")
+    @CrossOrigin(origins = ["http://localhost:8081", "https://trackr.wtf"])
+    fun consultarCorreios(@PathVariable("codigoRastreio") codigoRastreio: String): EntregaDto =
+            EntregasMapper.map(
+                    correiosCrawler.consultarEntrega(codigoRastreio),
+                    "",
+                    codigoRastreio
+            )
 }
